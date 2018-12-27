c7a57e5a90776b772662db9e359230832fe2643a
ayarbs89 <45944925+ayarbs89@users.noreply.github.com>
2018-12-27 16:26:53 -0500

    Update main_grading.py

--- main_grading.py
+++ main_grading.py
@@ -1,37 +1,65 @@
 #This is the official copy for grading picks
-import csv
-spread = {}
-with open('C:\\Spread_csv.csv') as Spread_csv:
-    Spread_csv_reader = csv.reader(Spread_csv, delimiter=',')
-    # build the Spread Dictionary - dont get this actually
-    for row in Spread_csv_reader:
-        spread[row[0]] = row[1]
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-picks = []
-with open('C:\\Picks1.csv') as players_file:
-    Players_csv_reader = csv.reader(players_file, delimiter=',')
 
-    # build the Players picks list
-    for row in Players_csv_reader:
-        picks.append(row)
 
-    # for row in Players_csv_reader:
-    #     score = row[int(4)]
-    #     values = [row[0], row[1], row[2], row[3], score]
-    #     picks.append(values)
-        # picks.append(row)
+# The ID and range of a sample spreadsheet.
+# SAMPLE_SPREADSHEET_ID = '1YhJdB8q0lgLi9lyRZ8CcUSI_1g66xCxJvNC5BVRVv_o'
+# SAMPLE_RANGE_SPREAD = 'Week 16!I:J'
 
-for p in picks:
-    score = float(p[4])
-    score += float(spread[p[1]])
-    score += float(spread[p[2]])
-    score += float(spread[p[3]])
-    p[4] = score
-    print (score)
-print (picks)
-        #Grades_csv.w
-        #Grades_csv.write("\n")
-with open('C:\\grades.csv', 'w') as Grades_csv:
-    Grades_csv_writer = csv.writer(Grades_csv, lineterminator='\n')
-    Grades_csv_writer.writerows(picks)
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+gc = gspread.authorize(credentials)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+#sheet = 'week 16'
+wks = gc.open_by_key('1YhJdB8q0lgLi9lyRZ8CcUSI_1g66xCxJvNC5BVRVv_o')
+sheet = wks.worksheet("Week 16")
+#Spread_Dictionary =
+ExcelSheet = sheet.get_all_values()
+d = {}
+
+for i in ExcelSheet[1:]:
+    if( i[8] != "" and i[9] != "" ):
+        d[i[8]] = float(i[9])
+    # for cell in line:
+    #     d[cell[1]] = cell[3]
+print(d)
+    #spread.__getitem__(row.items())
+
+#print(spread)
+
+
+# for cols in Spread_Dictionary:
+#     spread[cols[8]] = cols[9]
+#for cell in Spread_Dictionary
+
+#print(ExcelSheet)
+
+
+
+#def main():
+    #"""Shows basic usage of the Sheets API.
+    #Prints values from a sample spreadsheet.
+    #"""
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    # scope = ['https://www.googleapis.com/auth/spreadsheets']
+    # creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    # client = gspread.authorize(creds)
+    #
+    # # Find a workbook by name and open the first sheet
+    # # Make sure you use the right name here.
+    # sheet1 = 'week 16'
+    # sheet = client.open("20183Pick Standings").week16
+    # list_of_hashes = sheet.get_all_records()
+    # print (list_of_hashes)
+
+
+#if __name__ == '__main__':
+   # main()
+
 
